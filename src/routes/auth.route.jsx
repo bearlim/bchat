aff28bfd9d8fbbd42d37c9e9147bc09d434cfbb1
bearlim <brutchuces@outlook.com>
2022-07-19 21:41:52 -0300

    [Update] Login Facebook

--- src/routes/auth.route.jsx
+++ src/routes/auth.route.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import LoginScreen from "../screens/login/loginScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeLoginScreen from "../screens/homeLogin/homeLoginScreen";
+import { LoginManager, AccessToken } from "react-native-fbsdk-next";
+import { Auth } from "firebase/auth";
 
 const AuthStack = createStackNavigator();
 
@@ -15,6 +17,25 @@ const AuthRoutes = () => {
       <AuthStack.Screen name="HomeLoginScreen" component={HomeLoginScreen} />
     </AuthStack.Navigator>
   );
+};
+
+export const signInRequestFacebook = async () => {
+  const result = await LoginManager.logInWithPermissions([
+    "public_profile",
+    "email",
+  ]);
+
+  if (result.isCancelled) throw "Autenticação cancelada";
+
+  const data = await AccessToken.getCurrentAccessToken();
+
+  if (!data) throw "Alguma coisa deu errado, por favor, faça um issue";
+
+  const facebookCredential = auth.FacebookAuthProvider.credential(
+    data.accessToken
+  );
+
+  return Auth().signInWithCredential(facebookCredential);
 };
 
 export default AuthRoutes;
